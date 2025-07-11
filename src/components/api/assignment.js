467ab76399d0795d8cfda486fca1e2efc91c881d
joha <johahossainm@gmail.com>
2025-07-11 09:46:20 +0600

    assistant call create, assinment page call create,particular assistant call , post data with call complete

--- src/components/api/assignment.js
+++ src/components/api/assignment.js
@@ -1,19 +1,16 @@
 import axios from "axios";
+
 const API_BASE_URL = "https://aflieh-django.onrender.com/api";
 const token = localStorage.getItem("access_token");
 
 export const getAllAssignment = async () => {
   try {
-    const response = await fetch(`${API_BASE_URL}/cms/assignment/`, {
-      method: "GET",
+    const response = await axios.get(`${API_BASE_URL}/cms/assignment/`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    if (!response.ok) {
-      throw new Error("Failed to fetch Assistant");
-    }
-    return await response.json();
+    return response.data; // axios automatically parses JSON
   } catch (error) {
     console.error("Error fetching Assistant:", error);
     throw error;
